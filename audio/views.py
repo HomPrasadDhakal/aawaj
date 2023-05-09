df1ec0c5b5b311ec19b3cd5e7b57ac3d97164ff0
hom prasad dhakal <homprasaddhakal@gmail.com>
2023-05-10 01:12:53 +0545

    FIX: remove request import

--- audio/views.py
+++ audio/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status, viewsets, mixins
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-import requests
+
 from audio.serializers import (
     AllAudoSerializers,
 
